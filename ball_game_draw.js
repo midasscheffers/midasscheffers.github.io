0225ec0b2a1b0f36a4350af982ca4d8d1f65617c
Midas Scheffers <midasscheffers@gmail.com>
2017-11-01 14:01:53 +0100

    Disable zoom in game

--- ball_game_draw.js
+++ ball_game_draw.js
@@ -23,6 +23,22 @@ if (eerder != "ja"){
 }
 highscore = localStorage.getItem("highscore");
 
+$(document).keydown(function(event) {
+if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+        event.preventDefault();
+     }
+    // 107 Num Key  +
+    // 109 Num Key  -
+    // 173 Min Key  hyphen/underscor Hey
+    // 61 Plus key  +/= key
+});
+
+$(window).bind('mousewheel DOMMouseScroll', function (event) {
+       if (event.ctrlKey == true) {
+       event.preventDefault();
+       }
+});
+
 function setup(){
   createCanvas(xSize, ySize);
   noCursor();
