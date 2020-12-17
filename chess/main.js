fc7d0507eea8577da47b7df20a4815f2d69394f3
Midas Scheffers <midasscheffers@gmail.com>
2020-12-17 21:02:00 +0100

    [change], make chess icons appear in middle of squerre

--- chess/main.js
+++ chess/main.js
@@ -47,7 +47,7 @@ function draw() {
 
   pieces.forEach(p => {
     p.blit();
-    image(black_pon_img, p.x*spacing-halfBoard+halfSpace, p.y*spacing-halfBoard+halfSpace, 40, 40);
+    image(black_pon_img, p.x*spacing-halfBoard+spacing, p.y*spacing-halfBoard+spacing, spacing, spacing);
     if (CheckIfSelected(p)){
       pieces.forEach(j => {
         j.isSelected = false
