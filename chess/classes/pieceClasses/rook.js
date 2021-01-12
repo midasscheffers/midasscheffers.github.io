0e2d27c4824e748f68517bf55b4d0b65f0e0e836
Midas Scheffers <midasscheffers@gmail.com>
2021-01-12 10:00:09 +0100

    [add] movement for rook

--- chess/classes/pieceClasses/rook.js
+++ chess/classes/pieceClasses/rook.js
@@ -15,4 +15,30 @@ class Rook extends Piece{
     image(this.image, this.x*spacing-halfBoard, this.y*spacing-halfBoard, spacing, spacing);
   }
 
+  CheckMoveSqueres(boardState){
+    var moveSqueres = [];
+    for (let dir_y = -1; dir_y < 2; dir_y ++){
+      for (let dir_x = -1; dir_x < 2; dir_x ++){
+        if(abs(dir_x) != abs(dir_y)){
+          let inc = 1;
+          if (this.cordOnBoard(this.x+inc*dir_x, this.y+inc*dir_y)){
+            let pos_char = boardState[this.y+inc*dir_y][this.x+inc*dir_x];
+            while ((this.cordOnBoard(this.x+inc*dir_x, this.y+inc*dir_y)) && (this.charIsOppColor(pos_char) || pos_char == "-")){
+              moveSqueres.push([this.x+inc*dir_x, this.y+inc*dir_y]);
+              if(this.charIsOppColor(pos_char)){
+                break;
+              }
+              inc ++;
+              if (this.cordOnBoard(this.x+inc*dir_x, this.y+inc*dir_y)){
+                pos_char = boardState[this.y+inc*dir_y][this.x+inc*dir_x];
+              }
+            }
+          }
+        }
+      }
+    }
+    this.moveSqueres = moveSqueres;
+    return moveSqueres;
+  }
+
 }
