7f0a1ed8aeab3531ead6a234877b714776263620
Midas Scheffers <midasscheffers@gmail.com>
2021-01-12 13:06:07 +0100

    [add] movement for the king

--- chess/classes/pieceClasses/king.js
+++ chess/classes/pieceClasses/king.js
@@ -15,4 +15,28 @@ class King extends Piece{
     image(this.image, this.x*spacing-halfBoard, this.y*spacing-halfBoard, spacing, spacing);
   }
 
+  CheckMoveSqueres(boardState){
+    var moveSqueres = [];
+    for (let dir_y = -1; dir_y < 2; dir_y ++){
+      for (let dir_x = -1; dir_x < 2; dir_x ++){
+        if(dir_x != 0 || dir_y != 0){
+          let inc = 1;
+          if (this.cordOnBoard(this.x+inc*dir_x, this.y+inc*dir_y)){
+            let pos_char = boardState[this.y+inc*dir_y][this.x+inc*dir_x];
+            if ((this.cordOnBoard(this.x+inc*dir_x, this.y+inc*dir_y)) && (this.charIsOppColor(pos_char) || pos_char == "-")){
+              moveSqueres.push([this.x+inc*dir_x, this.y+inc*dir_y]);
+              if(this.charIsOppColor(pos_char) && pos_char != "-"){
+                break;
+              }
+              if (this.cordOnBoard(this.x+inc*dir_x, this.y+inc*dir_y)){
+                pos_char = boardState[this.y+inc*dir_y][this.x+inc*dir_x];
+              }
+            }
+          }
+        }
+      }
+    }
+    this.moveSqueres = moveSqueres;
+    return moveSqueres;
+  }
 }
